5d5bcbd886c414f5faa7597b21bb675f0c72d51a
sna2485 <saloni.aggarwal2509@gmail.com>
2022-03-20 23:57:00 -0400

    made changes to ReportingStructureImpl.java

--- src/main/java/com/mindex/challenge/service/impl/ReportingStructureImpl.java
+++ src/main/java/com/mindex/challenge/service/impl/ReportingStructureImpl.java
@@ -1,14 +1,11 @@
 package com.mindex.challenge.service.impl;
 
-import com.mindex.challenge.dao.EmployeeRepository;
 import com.mindex.challenge.data.Employee;
 import com.mindex.challenge.data.ReportingStructure;
 import com.mindex.challenge.service.EmployeeService;
 import com.mindex.challenge.service.ReportingStructureService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.util.UUID;
-import java.util.List;
 
 @Service
 public class ReportingStructureImpl implements ReportingStructureService {
@@ -23,8 +20,6 @@ public class ReportingStructureImpl implements ReportingStructureService {
         Employee employee = employeeService.read(employeeId);
         int totalReps = employeeService.totalReports(employeeId);
 
-        ReportingStructure reportingStructure = new ReportingStructure(employee, totalReps);
-
-        return reportingStructure;
+        return new ReportingStructure(employee, totalReps);
     }
 }
